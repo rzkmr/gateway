16b0ad3460d5450ba052993e4818c8f115d71e4c
Rzkmr <raj.shrestha@sproutify.com>
2017-02-17 09:31:29 +0545

    add dynamic routes

---
+++ app/models/dynamic_router.rb
@@ -0,0 +1,27 @@
+class DynamicRouter
+  def self.load
+    # Routes table does not exist at initial app setup and causes to stop db migration
+    return unless ActiveRecord::Base.connection.data_source_exists? 'routes'
+
+    Rails.application.routes.draw do
+      # todo: check for better route generator
+      Route.all.each do |route|
+        url_pattern = "api/#{route.version}/#{route.url_pattern}"
+        case route.verb
+        when 'get'
+          get url_pattern, to: 'gateways#call'
+        when 'post'
+          post url_pattern, to: 'gateways#call'
+        when 'put'
+          put url_pattern, to: 'gateways#call'
+        when  'delete'
+          delet url_pattern, to: 'gateways#call'
+        end
+      end
+    end
+  end
+
+  def self.reload
+    Rails.application.routes_reloader.reload!
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources 'services'
   resources 'routes'
+
+  DynamicRouter.load
 end
